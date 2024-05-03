0fbadd91961c7464f60f582ee8e315dcbbedf6d4
lostmart <martinpedraza1979@gmail.com>
2024-05-03 21:46:49 +0200

    new accordion

--- src/components/atoms/Accordion.jsx
+++ src/components/atoms/Accordion.jsx
@@ -4,21 +4,47 @@ import { useRef, useState } from "react"
 import { ChevronUp } from "react-feather"
 
 const Accordion = ({ title, body, extraClass }) => {
+	const bodyRef = useRef(null)
+	const arrowRef = useRef(null)
+
+	const [active, setActive] = useState(false)
+
+	const handleClick = () => {
+		const panel = bodyRef.current
+		const arrow = arrowRef.current
+
+		if (active) {
+			setActive(!active)
+			panel.style.maxHeight = "0"
+			panel.style.padding = "0 16px 0 16px"
+			arrow.style.rotate = "0deg"
+		} else {
+			setActive(!active)
+			panel.style.maxHeight = panel.scrollHeight + "px"
+			panel.style.padding = "18px 16px 12px 16px"
+			arrow.style.rotate = "180deg"
+		}
+	}
 	return (
 		<div className="accordion">
 			<h3 className="accordion__header">
-				<button>
+				<button className="accordion__button" onClick={handleClick}>
 					<span>
 						<span>JavaScript React Developer</span>
-						<span className="titlesSection_level">
-							Diplôme de niveau 6 (bac +4)
-						</span>
 					</span>
 
-					<ChevronUp />
+					<ChevronUp ref={arrowRef} />
 				</button>
 			</h3>
-			<div className="accordion__body">Accordion body ...</div>
+			<div ref={bodyRef} className="accordion__body">
+				<h4 className="titlesSection_level">Diplôme de niveau 6 (bac +4)</h4>
+				<p className="accordion__content">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta sit
+					accusantium, facere officia omnis minima molestias repellendus
+					officiis dignissimos, quos ipsa ratione necessitatibus iusto sed
+					commodi voluptatum architecto nesciunt.
+				</p>
+			</div>
 		</div>
 	)
 }
