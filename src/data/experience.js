febb273e26b0d8acaba518adc890810918e2cee7
lostmart <martinpedraza1979@gmail.com>
2024-05-16 20:00:26 +0200

    new experience added

---
+++ src/data/experience.js
@@ -0,0 +1,36 @@
+const experienceData = [
+	{
+		institute: ["ENSEA", "France"],
+		title: "Full-stack web development instructor",
+		date: "September 2023 - Present",
+		link: "",
+		areas: [
+			"<span class='card_detail'>Project Creation and Demonstration:</span><span>Design and implement comprehensive projects to showcase the advantages and practical applications of working with REST APIs, demonstrating real-world scenarios and solutions.</span>",
+			"<span class='card_detail'>Promotion of Self-Learning:</span><span>Encourage and guide students in adopting self-learning techniques and best practices, fostering independent problem-solving skills and continuous personal development.</span>",
+			"<span class='card_detail'>API Development:</span><span>Develop robust and scalable APIs using Python and Node.js, ensuring efficient data handling and seamless integration with various client applications.</span>",
+			"<span class='card_detail'>Front-End and CLI Development:</span><span>Create intuitive front-end applications and command-line interfaces (CLI) that consume and interact with both internal and third-party API endpoints, enhancing user experience and functionality.</span>",
+			"<span class='card_detail'>External API Integration:</span><span>Manage and utilize resources from external APIs, integrating their functionalities into projects to extend capabilities and provide richer features.</span>",
+			"<span class='card_detail'>Mentorship and Support:</span><span>Provide mentorship and support to students, offering guidance on project development, debugging, and best coding practices.</span>",
+		],
+		conclusion:
+			"This role involves a blend of technical expertise and educational mentorship, equipping students with the necessary skills to excel in modern web development.",
+	},
+	{
+		institute: ["OpenClassrooms.com", "France, UK, USA"],
+		title: "Full-stack web development instructor",
+		date: "September 2023 - Present",
+		link: "",
+		areas: [
+			"<span class='card_detail'>Project Creation and Demonstration:</span><span>Design and implement comprehensive projects to showcase the advantages and practical applications of working with REST APIs, demonstrating real-world scenarios and solutions.</span>",
+			"<span class='card_detail'>Promotion of Self-Learning:</span><span>Encourage and guide students in adopting self-learning techniques and best practices, fostering independent problem-solving skills and continuous personal development.</span>",
+			"<span class='card_detail'>API Development:</span><span>Develop robust and scalable APIs using Python and Node.js, ensuring efficient data handling and seamless integration with various client applications.</span>",
+			"<span class='card_detail'>Front-End and CLI Development:</span><span>Create intuitive front-end applications and command-line interfaces (CLI) that consume and interact with both internal and third-party API endpoints, enhancing user experience and functionality.</span>",
+			"<span class='card_detail'>External API Integration:</span><span>Manage and utilize resources from external APIs, integrating their functionalities into projects to extend capabilities and provide richer features.</span>",
+			"<span class='card_detail'>Mentorship and Support:</span><span>Provide mentorship and support to students, offering guidance on project development, debugging, and best coding practices.</span>",
+		],
+		conclusion:
+			"This role involves a blend of technical expertise and educational mentorship, equipping students with the necessary skills to excel in modern web development.",
+	},
+]
+
+export default experienceData
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,23 +1,43 @@
 import React from "react"
+import experienceData from "../data/experience"
 
 const Experience = () => {
+	const ExperienceDataRender = () => {
+		return experienceData.map((exp) => {
+			return (
+				<article key={exp.title} className="card">
+					<div>
+						<h4>
+							<a target="_blank" href="https://www.ensea.fr/fr">
+								{exp.institute[0]}
+							</a>
+							, {exp.institute[1]} —
+							<span className="card_title">{exp.title}</span>
+						</h4>
+						<span className="card_date">{exp.date}</span>
+						<ul>
+							{exp.areas.map((area) => {
+								return (
+									<li
+										key={area}
+										dangerouslySetInnerHTML={{ __html: area }}
+									></li>
+								)
+							})}
+						</ul>
+						<p>{exp.conclusion}</p>
+					</div>
+				</article>
+			)
+		})
+	}
 	return (
 		<section className="experience">
 			<h2 className="page_title">
 				<span>P</span>rofessional <span>E</span>xperience
 			</h2>
 			<div>
-				<article className="card">
-					<div>
-						<img className="courses_imgs" src="" alt="ok" />
-						<h4>
-							<span>la vida</span>
-							<span className="course_year"> 123456 </span>
-						</h4>
-					</div>
-
-					<button>Show Title</button>
-				</article>
+				<ExperienceDataRender />
 			</div>
 		</section>
 	)
