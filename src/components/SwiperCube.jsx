00743548e59240db2b0dab3a27e1bc1bfb42f3be
lostmart <martinpedraza1979@gmail.com>
2024-05-29 18:44:11 +0200

    new slides styles

--- src/components/SwiperCube.jsx
+++ src/components/SwiperCube.jsx
@@ -1,12 +1,22 @@
 import { Swiper, SwiperSlide } from "swiper/react"
+import { MdOutlineSwipeLeft } from "react-icons/md"
 
 // Import Swiper styles
 import "swiper/css"
 import "swiper/css/effect-cube"
 
 import { Autoplay, EffectCube } from "swiper/modules"
+import { LogIn } from "react-feather"
+import { useState } from "react"
 
 export default function SwiperCube({ details } = props) {
+	const [showHelp, setShowHelp] = useState(true)
+
+	const handleDrag = () => {
+		setShowHelp(() => {
+			//return false
+		})
+	}
 	return (
 		<>
 			<Swiper
@@ -23,13 +33,22 @@ export default function SwiperCube({ details } = props) {
 					shadowOffset: 20,
 					shadowScale: 0.94,
 				}}
+				// onSlideChange={handleDrag}
 				modules={[EffectCube, Autoplay]}
 				className="mySwiper"
 			>
-				{details.map((detail) => {
+				{details.map((detail, indx) => {
 					return (
 						<SwiperSlide key={detail.imgName}>
 							<figure>
+								{indx === 0 && showHelp ? (
+									<div className="btn-container animate__headShake animate__animated">
+										<MdOutlineSwipeLeft />
+									</div>
+								) : (
+									""
+								)}
+
 								<img src={`./assests/${detail.imgName}`} alt={detail.legend} />
 								<figcaption>{detail.legend}</figcaption>
 							</figure>
